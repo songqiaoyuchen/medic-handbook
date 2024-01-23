f9d683ed7bef2f7a5ecaa5b460d6036e65cf6712
songqiaoyuchen <155654285+songqiaoyuchen@users.noreply.github.com>
2024-01-23 19:46:36 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,28 @@
-var keys = {37: 1, 38: 1, 39: 1, 40: 1};
-
-function preventDefault(e) {
-  e.preventDefault();
-}
-
-function preventDefaultForScrollKeys(e) {
-  if (keys[e.keyCode]) {
-    preventDefault(e);
-    return false;
-  }
-}
-
-// modern Chrome requires { passive: false } when adding event
-var supportsPassive = false;
-try {
-  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-    get: function () { supportsPassive = true; } 
-  }));
-} catch(e) {}
-
-var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
-// call this to Disable
-function disableScroll() {
-  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-}
-
-// call this to Enable
-function enableScroll() {
-  window.removeEventListener('DOMMouseScroll', preventDefault, false);
-  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
-  window.removeEventListener('touchmove', preventDefault, wheelOpt);
-  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
-}
+window.addEventListener('scroll', () => {
+  document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+})
 
 function toggleNav(x) {
     if(document.getElementById("sideNav").style.width === "0px"){
       document.getElementById("sideNav").style.width = "200px";
-      disableScroll()
+      const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+      const body = document.body;
+      body.style.position = 'fixed';
+      body.style.top = `-${scrollY}`;
     }else{
       document.getElementById("sideNav").style.width = "0px";
-      document.getElementsByTagName("main")[0].style.height = "auto"
-      enableScroll()
+      const body = document.body;
+      const scrollY = body.style.top;
+      body.style.position = '';
+      body.style.top = '';
+      window.scrollTo(0, parseInt(scrollY || '0') * -1);
     }
     x.classList.toggle("change");
   }
 
   function toggleDropdown1(x) {
     if(document.getElementById("firstDrop").style.height === "0px"){
-      document.getElementById("firstDrop").style.height = "120px"
+      document.getElementById("firstDrop").style.height = "113px"
     }else{
       document.getElementById("firstDrop").style.height = "0px";
     }
@@ -60,7 +30,7 @@ function toggleNav(x) {
 
   function toggleDropdown2(x) {
     if(document.getElementById("secondDrop").style.height === "0px"){
-      document.getElementById("secondDrop").style.height = "120px"
+      document.getElementById("secondDrop").style.height = "113px"
     }else{
       document.getElementById("secondDrop").style.height = "0px";
     }
@@ -68,7 +38,7 @@ function toggleNav(x) {
 
   function toggleDropdown3(x) {
     if(document.getElementById("thirdDrop").style.height === "0px"){
-      document.getElementById("thirdDrop").style.height = "80px"
+      document.getElementById("thirdDrop").style.height = "113px"
     }else{
       document.getElementById("thirdDrop").style.height = "0px";
     }
