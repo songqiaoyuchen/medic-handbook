023f63902b855a5e4bb87d7890b09cae61ee95d6
SQ Yuchen <155654285+songqiaoyuchen@users.noreply.github.com>
2024-01-28 02:03:08 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,65 +3,80 @@ window.addEventListener('scroll', () => {
 })
 
 function toggleNav(x) {
-  if(document.getElementById("sideNav").style.width === "0px" || !document.getElementById("sideNav").style.width) {
-    document.getElementById("sideNav").style.width = "200px";
+  var sideNav = document.getElementById("sideNav")
+  if(sideNav.style.width === "0px" || !sideNav.style.width) {
+    sideNav.style.width = "200px";
     const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
     const body = document.body;
     body.style.position = 'fixed';
     body.style.top = `-${scrollY}`;
+    if(document.getElementById("pageDir").style.width !== "0px") {
+      document.getElementById("pageDir").style.width = "0px";
+      document.getElementById("dirToggler").src = "./assets/icons/bookmarks.svg";
+      document.getElementById("pageDir").classList.toggle("show-dir-border")
+    }
   }else{
-    document.getElementById("sideNav").style.width = "0px";
+    sideNav.style.width = "0px";
     const body = document.body;
     const scrollY = body.style.top;
     body.style.position = '';
     body.style.top = '';
     window.scrollTo(0, parseInt(scrollY || '0') * -1);
   }
-  document.getElementById("sideNav").classList.toggle("show-sidenav");
+  sideNav.classList.toggle("show-sidenav");
   x.classList.toggle("change");
 }
 
 function toggleDir(x) {
-  if (document.getElementById("pageDir").style.width === "0px" || !document.getElementById("pageDir").style.width) {
-    document.getElementById("pageDir").style.width = "auto";
+  var pageDir = document.getElementById("pageDir")
+  var dirToggler = document.getElementById("dirToggler")
+  if (pageDir.style.width === "0px" || !pageDir.style.width) {
+    pageDir.style.width = "300px";
+    dirToggler.src = "./assets/icons/bookmarks-fill.svg";
     const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
     const body = document.body;
     body.style.position = 'fixed';
     body.style.top = `-${scrollY}`;
+    if(document.getElementById("sideNav").style.width !== "0px") {
+      document.getElementById("sideNav").style.width = "0px";
+      document.getElementById("navbarToggler").classList.toggle("change")
+      document.getElementById("sideNav").classList.toggle("show-sidenav");
+    }
   }else{
-    document.getElementById("pageDir").style.width = "0px";
+    pageDir.style.width = "0px";
+    dirToggler.src = "./assets/icons/bookmarks.svg";
     const body = document.body;
     const scrollY = body.style.top;
     body.style.position = '';
     body.style.top = '';
     window.scrollTo(0, parseInt(scrollY || '0') * -1);
   }
-  document.getElementById("pageDir").classList.toggle("show-dir");
+  pageDir.classList.toggle("show-dir-border");
 }
 
-  function toggleRefDropdown() {
-    if(document.getElementById("refDropdown").style.height === "175.56px"){
-      document.getElementById("refDropdown").style.height = "0px"
-    }else{
-      document.getElementById("refDropdown").style.height = "175.56px";
-    }
+function toggleRefDropdown() {
+  if(document.getElementById("refDropdown").style.height === "175.56px"){
+    document.getElementById("refDropdown").style.height = "0px"
+  }else{
+    document.getElementById("refDropdown").style.height = "175.56px";
   }
+}
 
-  function toggleGuideDropdown() {
-    if(document.getElementById("guideDropdown").style.height === "175.56px"){
-      document.getElementById("guideDropdown").style.height = "0px"
-    }else{
-      document.getElementById("guideDropdown").style.height = "175.56px";
-    }
+function toggleGuideDropdown() {
+  if(document.getElementById("guideDropdown").style.height === "175.56px"){
+    document.getElementById("guideDropdown").style.height = "0px"
+  }else{
+    document.getElementById("guideDropdown").style.height = "175.56px";
   }
+}
 
-  function toggleAdminDropdown() {
-    if(document.getElementById("adminDropdown").style.height === "122.38px"){
-      document.getElementById("adminDropdown").style.height = "0px"
-    }else{
-      document.getElementById("adminDropdown").style.height = "122.38px";
-    }
+function toggleAdminDropdown() {
+  if(document.getElementById("adminDropdown").style.height === "122.38px"){
+    document.getElementById("adminDropdown").style.height = "0px"
+  }else{
+    document.getElementById("adminDropdown").style.height = "122.38px";
   }
+}
 
   
   
